65648d0074bb03deb1c7468bd057ea36c369295f
vovaproger <uladzimir.charniauski@gmail.com>
2024-08-27 09:14:26 -0400

    adding files

---
+++ decrypt_message_one.py
@@ -0,0 +1,43 @@
+cipher = {
+    'a':'v',
+    'b':'h',
+    'c':'r',
+    'd':'j',
+    'e':'t',
+    'f':'x',
+    'g':'s',
+    'h':'a',
+    'i':'e',
+    'j':'f',
+    'k':'b',
+    'l':'n',
+    'm':'o',
+    'n':'i',
+    'o':'g',
+    'p':'l',
+    'q':'m',
+    'r':'z',
+    's':'q',
+    't':'u',
+    'u':'c',
+    'v':'k',
+    'w':'d',
+    'x':'p',
+    'y':'y',
+    'z':'w',
+    ' ': '}',
+    '\n': '^',
+    ',': '5',
+    '!': '[',
+    ':':'-',
+    ')':'*',
+    '.': '%' 
+}
+
+encrypted_file = open("encrypted_message_one.txt", 'r')
+
+encrypted_message = encrypted_file.readline()
+
+encrypted_file.close()
+
+# Write code below
---
+++ decrypt_message_two.py
@@ -0,0 +1,7 @@
+encrypted_file = open("encrypted_message_two.txt", 'r')
+
+encrypted_message = encrypted_file.readline()
+
+encrypted_file.close()
+
+# Write Code Here
